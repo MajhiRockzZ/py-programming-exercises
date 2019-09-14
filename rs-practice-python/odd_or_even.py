cd1c088dc5860c5ec199ea6d7d4620c92ab7485e
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-09-14 08:56:33 +0530

    Update odd_or_even.py

--- rs-practice-python/odd_or_even.py
+++ rs-practice-python/odd_or_even.py
@@ -20,3 +20,33 @@ if num % 2 == 0:
     print(f"{num} is a even number.")
 else:
     print(f"{num} is a odd number.")
+
+
+""" 
+-------------------------------------------------------------
+Extras :
+-------------------------------------------------------------
+1. If the number is a multiple of 4, print out a different
+    message
+
+2. Ask the user for two numbers : one number to check and one
+    number to divided by. If check divides evenly, tell that 
+    to the user. If not, print a different appropriate message
+-------------------------------------------------------------
+ """
+
+if num % 4 == 0:
+    print(f"{num} is a even number and divisible by 4")
+elif num % 2 == 0:
+    print(f"{num} is even number and divisible by 2")
+else:
+    print(f"{num} is a odd number")
+
+
+check = int(input(f"Enter a number of choice to CHECK? : \n"))
+num_to_divide = int(input(f"Enter a number to divide by? : \n"))
+
+if num_to_divide % check == 0:
+    print(f"{check} divides {num_to_divide} evenly.")
+else:
+    print(f"{check} divides {num_to_divide} unevenly.")
