ab3443330cad3aa280105c6d40f053d84d91fd5f
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-09-16 19:47:52 +0530

    feat: Add  list overlap

---
+++ rs-practice-python/05_list_overlap.py
@@ -0,0 +1,18 @@
+""" 
+---------------------------------------------------------
+Exercise 5 : 
+---------------------------------------------------------
+Take two lists, and write a program that returns a list
+that contains only the elements that are common between
+the list (without duplicate). Make sure your program 
+works on two lists of different sizes.
+---------------------------------------------------------
+Concepts : 
+---------------------------------------------------------
+1. List properties --> eg: >> a = [1, 2, 3, 4]
+  >> 10 in a --> False >> 3 in a --> True
+----------------------------------------------------------
+ """
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
