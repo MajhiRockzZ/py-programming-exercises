3d277b49c8dc20efdc39e0dd6860b313a03eb11a
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-09-16 21:37:20 +0530

    feat: Add list overlay

--- rs-practice-python/05_list_overlap.py
+++ rs-practice-python/05_list_overlap.py
@@ -16,3 +16,13 @@ Concepts :
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+result = []
+
+for a_element in a:
+    # print(a_element)
+    if a_element in b:
+        result.append(a_element)
+
+
+print(set(result))
