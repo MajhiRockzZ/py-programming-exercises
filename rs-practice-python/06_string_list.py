1d61fb30580a39d9ca26fb2b241826ca46d5190a
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-09-21 01:00:18 +0530

    feat: Add string list problem

---
+++ rs-practice-python/06_string_list.py
@@ -0,0 +1,19 @@
+""" 
+---------------------------------------------------------
+Exercise 5 : 
+---------------------------------------------------------
+Ask the user for a string and print out whether this 
+string is a palindrome or not. (A palindrome is a string
+that reads the same forwards and backwards.)
+---------------------------------------------------------
+Concepts : 
+---------------------------------------------------------
+1. List Indexing --> We starts counting lists from the
+  number 0.
+2. Strings are lists --> We can do to string everything
+  that we can do to list.
+----------------------------------------------------------
+ """
+
+name = "Sumesh"
+print(f"Hello {name}")
