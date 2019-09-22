17790ca66ad95270b4eec32190b0b912bbdee4ba
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-09-22 20:39:33 +0530

    feat: Add solution for list comprehensions

--- rs-practice-python/07_list_comprehensions.py
+++ rs-practice-python/07_list_comprehensions.py
@@ -15,3 +15,29 @@ Concepts :
   eg: ages = [2014 - year for year in year_of_birth]
 ----------------------------------------------------------
  """
+import random
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+b = [element for element in a if element % 2 == 0]
+print(b)
+
+""" 
+---------------------------------------------------------
+<<<<<<<------- Alternative Solution ------->>>>>>>>>
+---------------------------------------------------------
+ """
+
+numlist = []
+list_length = random.randint(5, 15)
+
+print(list_length)
+
+while len(numlist) < list_length:
+    numlist.append(random.randint(1, 75))
+
+
+# print(numlist)
+
+evenlist = [number for number in numlist if number % 2 == 0]
+
+print(numlist)
+print(evenlist)
