3e67d67cca373b85df4ab45fd1cdde75e149303e
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-09-25 21:38:36 +0530

    feat: Add problem for rpc game

---
+++ rs-practice-python/08_rock_paper_scissors.py
@@ -0,0 +1,23 @@
+""" 
+---------------------------------------------------------
+Exercise 8: 
+---------------------------------------------------------
+Make a two-player Rock-Paper-Scissors game.
+Rules: 
+    Rock beats scissors
+    Scissors beats paper
+    Paper beats rock
+---------------------------------------------------------
+---------------------------------------------------------
+Hint : Ask  for player plays (using input), compare them,
+  print out a message of congratulation to the winner, 
+  and ask if the player want to start a new game.
+---------------------------------------------------------
+---------------------------------------------------------
+Concepts : 
+---------------------------------------------------------
+1. While loops
+2. Infinite loops
+3. Break statements
+----------------------------------------------------------
+ """
