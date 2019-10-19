570c1b865f0305ed1e4b283b1bd87fb91ba1ebe9
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-10-19 22:35:48 +0530

    Problem statement and example for check primality functions question

---
+++ rs-practice-python/11_check_primality_functions.py
@@ -0,0 +1,28 @@
+"""
+---------------------------------------------------------
+Exercise 11:
+---------------------------------------------------------
+Ask the user for a number and determine whether the number
+is prime or not. Take this opportunity to practice using
+functions
+---------------------------------------------------------
+Concepts :
+---------------------------------------------------------
+1. Functions
+2. Reusable functions
+3. Default arguments
+
+eg: def get_integer(help_text="Give me a number: "):
+    # -- Example for default arguments and function 👆--
+        return int(input(help_text))
+
+    # -- Example for reusable functions 👇 -- 
+    age = get_integer("Tell me your age: ")
+    school_year = get_integer()
+
+    if age > 15:
+        print(f"You are over the age of 15")
+
+    print(f"You are in grade {school_year}")
+----------------------------------------------------------
+ """
