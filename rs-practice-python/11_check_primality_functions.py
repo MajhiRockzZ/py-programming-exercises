443c22858fbe24e014ead7de94f4ba8db5f7a0b6
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-10-20 00:07:30 +0530

    feat: add solution for check primality func

--- rs-practice-python/11_check_primality_functions.py
+++ rs-practice-python/11_check_primality_functions.py
@@ -16,7 +16,7 @@ eg: def get_integer(help_text="Give me a number: "):
     # -- Example for default arguments and function 👆--
         return int(input(help_text))
 
-    # -- Example for reusable functions 👇 -- 
+    # -- Example for reusable functions 👇--
     age = get_integer("Tell me your age: ")
     school_year = get_integer()
 
@@ -26,3 +26,22 @@ eg: def get_integer(help_text="Give me a number: "):
     print(f"You are in grade {school_year}")
 ----------------------------------------------------------
  """
+
+def check_prime(input_value = "Enter a number to check : "):
+    int_input = int(input(input_value))
+
+    temp_input = int_input
+
+    counter = 0
+
+    while temp_input > 0:
+        if(int_input % temp_input == 0):
+            counter = counter + 1
+        temp_input = temp_input - 1
+
+    if counter == 2:
+        print(f"It's a prime number!")
+    else:
+        print(f"It's not a prime number!")
+
+check_prime()
