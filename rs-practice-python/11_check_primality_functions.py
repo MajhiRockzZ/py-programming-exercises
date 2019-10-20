72f67be9c022a57ae3e6caf3f8e94e7bb38a48a4
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-10-20 09:49:22 +0530

    check primality function alt sol 3

--- rs-practice-python/11_check_primality_functions.py
+++ rs-practice-python/11_check_primality_functions.py
@@ -98,3 +98,22 @@ elif number == 0:
     sys.exit("The number is not prime.")
 else:
     sys.exit("The number is not prime.")
+
+"""
+---------------------------------------------------------
+  <<<<<<<----- Alternative Solution : 3 ----->>>>>>>>>
+---------------------------------------------------------
+ """
+num = int(input('Insert a number: '))
+a = [x for x in range(2, num) if num % x == 0]
+
+def is_prime(n):
+    if num > 1:
+        if len(a) == 0:
+            print("prime")
+        else:
+            print("NOT prime")
+    else:
+        print("NOT prime")
+
+is_prime(num)
