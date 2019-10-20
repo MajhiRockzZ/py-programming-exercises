8aea289dd66d7d6ab6209b3247c6b502a6fef38f
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-10-20 09:33:32 +0530

    check primality functions sol 1

--- rs-practice-python/11_check_primality_functions.py
+++ rs-practice-python/11_check_primality_functions.py
@@ -45,3 +45,35 @@ def check_prime(input_value = "Enter a number to check : "):
         print(f"It's not a prime number!")
 
 check_prime()
+
+"""
+---------------------------------------------------------
+  <<<<<<<----- Alternative Solution : 1 ----->>>>>>>>>
+---------------------------------------------------------
+ """
+def get_number(prompt):
+    return int(input(prompt))
+
+def is_prime(number):
+    if number == 1:
+        prime = False
+    elif number == 2:
+        prime = True
+    else:
+        prime = True
+        for check_number in range(2, (number / 2)-1):
+            if number % check_number == 0:
+                prime = False
+                break
+    return prime
+
+def print_prime(number):
+    prime = is_prime(number)
+    if prime:
+        descriptor = ""
+    else:
+        descriptor = "not "
+    print(number, " is", descriptor, "prime.", sep = "", end = "\n\n")
+
+while 1 == 1:
+    print_prime(get_number("Enter a number to check. Ctl-C to exit."))
