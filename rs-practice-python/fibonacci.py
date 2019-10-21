0452df31e63cd4749311c11b4057ea312eac72e3
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-10-21 22:17:46 +0530

    feat: add problem statement for fibonacci

---
+++ rs-practice-python/fibonacci.py
@@ -0,0 +1,17 @@
+"""
+---------------------------------------------------------
+Exercise 13:
+---------------------------------------------------------
+Write a program that asks the user how many Fibonnaci
+numbers to generate and then generates them (using
+functions).Make sure to ask the user to enter the number
+of numbers in the sequence to generate
+---------------------------------------------------------
+Hint :
+---------------------------------------------------------
+The Fibonnaci sequence is a sequence of numbers where the
+next number in the sequence is the sum of the previous two
+numbers in the sequence. E.x : 1, 1, 2, 3, 5, 8, 13, ...
+---------------------------------------------------------
+ """
+
