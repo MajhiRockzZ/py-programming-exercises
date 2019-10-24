7ddafeb58ed63424018192b65fd0ab3745078bcc
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-10-24 08:05:16 +0530

    feat: 📦 add solution for fibonacci problem

--- rs-practice-python/fibonacci.py
+++ rs-practice-python/fibonacci.py
@@ -15,3 +15,23 @@ numbers in the sequence. E.x : 1, 1, 2, 3, 5, 8, 13, ...
 ---------------------------------------------------------
  """
 
+
+def gen_fib():
+    count = int(input(f"How many fibonacci numbers would you like to generate?"))
+
+    i = 1
+    if count == 0:
+        fib = []
+    elif count == 1:
+        fib = [1]
+    elif count == 2:
+        fib == [1, 1]
+    elif count > 2:
+        fib = [1, 1]
+        while i < (count - 1):
+            fib.append(fib[i] + fib[i-1])
+            i += 1
+    return fib
+
+
+print(gen_fib())
