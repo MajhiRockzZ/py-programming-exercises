78ef5f6f96688d0080de982f8f5e2158118b4358
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-10-28 21:48:27 +0530

    feat: ☢ Add problem Reverse Word Order

---
+++ rs-practice-python/15_reverse_word_order.py
@@ -0,0 +1,16 @@
+"""
+---------------------------------------------------------
+Exercise 15 :
+---------------------------------------------------------
+Write a program (using functions!) that asks the user for
+a long string containing multiple words. Print back to the
+user the same string, except with the words in backwrds
+order. eg: <<< My name is Michele
+            >>> Michele is name My
+---------------------------------------------------------
+Concepts :
+---------------------------------------------------------
+    1. More string things.
+---------------------------------------------------------
+ """
+
