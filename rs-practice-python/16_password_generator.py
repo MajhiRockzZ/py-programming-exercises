5b86e64c57c008f5836c50e712c45fe095170164
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-10-31 23:07:57 +0530

    feat: 📦 add solution1 for password generator

--- rs-practice-python/16_password_generator.py
+++ rs-practice-python/16_password_generator.py
@@ -5,8 +5,8 @@ Exercise 16 :
 Write a password generator in Python. Be creative with
 how you generate passwords-strong passwords have a mix
 of lowercase letters, uppercase letters, numbers, and
-symbols. The passwords should be random, generating a new
-password every time the user asks for a new password.
+symbols. The passwords should be random, generating a
+new password every time the user asks for a new password.
 Include your run-time code in a maine method.
 ---------------------------------------------------------
 Extra :
@@ -16,5 +16,13 @@ Extra :
     list.
 ---------------------------------------------------------
  """
+import random
 
+pass_combination = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+
+pass_length = 8
+
+pass_output = "".join(random.sample(pass_combination, pass_length))
+
+print(pass_output)
 
