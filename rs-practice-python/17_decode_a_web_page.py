e650f871845bb6503e9612bf112db650c39744b5
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-11-03 18:32:24 +0530

    feat: Add problem statement for decode a web page

---
+++ rs-practice-python/17_decode_a_web_page.py
@@ -0,0 +1,15 @@
+"""
+---------------------------------------------------------
+Exercise 17 :
+---------------------------------------------------------
+Use the BeautifulSoup and requests Python packages to
+print out a list of all the article titles on the New
+York Times homepage[].
+---------------------------------------------------------
+Concepts :
+---------------------------------------------------------
+    1. Libraries
+    2. requests
+    3. BeautifulSoup
+---------------------------------------------------------
+ """
