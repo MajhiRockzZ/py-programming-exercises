29b0708be710d664f447abe2c193ca204df37cf3
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-12-08 23:14:52 +0530

    feat: 💾 Add modules and packages

--- mega-python/26_modules_libraries_and_packages.py
+++ mega-python/26_modules_libraries_and_packages.py
@@ -1,4 +1,11 @@
-import os # Module's (os.__file__)
+import os  # Module's (os.__file__)
+import pickle
+import sqlite3
+import glob2
 
 os.listdir()
 dir(os)
+
+help(pickle)
+help(sqlite3)
+help(glob2)
